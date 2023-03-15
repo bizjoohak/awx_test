fc73e0fc5718d2e52013a635bce6ffda5735ec9e
bizjoohak <joohak@bizdata.kr>
2023-03-15 04:08:17 +0000

    biz

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>uuuuuuuuuuuuasfdsdsssssdsds</h1><input type="textbox"/>'
+    return '<h1>비즈데이터 좋아요!</h1><input type="textbox"/>'
 
 app.run(host='0.0.0.0', port=8888)
